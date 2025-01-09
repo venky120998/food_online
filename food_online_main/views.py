daf27ab863a57809c0cd9d719afdb5d3ee8dbf63
venky120998 <kvenkatganesh98@gmail.com>
2025-01-09 19:13:48 +0530

    homepage_setup, postgre config and .env sample

--- food_online_main/views.py
+++ food_online_main/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse(content= " Hello World!!!")
+    return render(request=request,template_name='home.html')
